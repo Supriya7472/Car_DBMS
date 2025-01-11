9a38ed4a3b99480500e6cbf09f59923030a12e84
Supriya7472 <147753391+Supriya7472@users.noreply.github.com>
2025-01-11 21:09:37 +0530

    Update DeleteCarByIdServlet.java

--- src/main/java/com/jsp/servlet/cardb/DeleteCarByIdServlet.java
+++ src/main/java/com/jsp/servlet/cardb/DeleteCarByIdServlet.java
@@ -24,7 +24,8 @@ public class DeleteCarByIdServlet extends HttpServlet{
 		Connection conn=null;
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet_cardb?user=root&password=Supriya@104");
+			conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/servlet_cardb?user=root&password=root");
+					//provide details of your user id and password inside query parameter
 			PreparedStatement pst=conn.prepareStatement("Delete from car where CarId=?");
 			pst.setInt(1, carId);
 
